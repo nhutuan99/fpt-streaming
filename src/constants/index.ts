d716078c9cc8a98f146b666aab5affcbb63b3362
vanminhquangtri <vanminhquangtri@gmail.com>
2024-12-24 09:23:14 +0700

    update link live

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,13 +3,13 @@ const channels = [
     name: "VTV1",
     id: "vtv-1",
     icon: "https://images.fptplay53.net/media/channels/icon_channel_vtv1-hd_165657381026.png?w=300&c=0&fmt=webp",
-    src: "https://vips-livecdn.fptplay.net/MTk3OTkyNXx2dHYxaGRfYWJyLnNtaWx8ZnB0fDE3MzQ5MjA3MDd8MjEuMjQwLjIxNy43NHwwNGQzNTk4NTQyMjE5MzMwOWZlZDcxMmQyMzk4ZWIxNw/fnxch2/vtv1hd_abr.smil/playlist.m3u8",
+    src: "https://vips-livecdn.fptplay.net/MTA4NDUxNDB8dnR2MWhkX2Fici5zbWlsfGZwdHwxNzM1MDA2ODY3fDIxLjI0MC4yMTcuNzR8MzI2YTZkOTI5ODRmM2U3MjgxMDI3NWRjYmQxNTczNjE/fnxch2/vtv1hd_abr.smil/playlist.m3u8",
   },
   {
     name: "VTV3",
     id: "vtv-3",
     icon: "https://images.fptplay53.net/media/channels/icon_channel_vtv3-hd_165657381668.png?w=300&c=0&fmt=webp",
-    src: "https://vips-livecdn.fptplay.net/MTA4NDUxNDB8dnR2M2hkX2Fici5zbWlsfGZwdHwxNzM0OTQyNjAxfDIxLjI0MC4yMTcuNzR8YjFlZDYxNGRiZWNkNDczMzNhNzNlYzI3NTU1ZjI0Mjg/fnxch2/vtv3hd_abr.smil/playlist.m3u8",
+    src: "https://vips-livecdn.fptplay.net/MTA4NDUxNDB8dnR2M2hkX2Fici5zbWlsfGZwdHwxNzM1MDA2OTI2fDIxLjI0MC4yMTcuNzR8MjFmNjE4YjEwYjRjNDNhNzVlNWQ4ZjdiMGQ1NTEzMTE/fnxch2/vtv3hd_abr.smil/playlist.m3u8",
   },
 ];
 
